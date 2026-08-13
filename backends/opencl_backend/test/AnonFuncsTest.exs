75279b89a51a255a95d346d467b4a103a5db1992
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-08-13 15:58:19 -0300

    Created polymorphic function test programs

--- backends/opencl_backend/test/AnonFuncsTest.exs
+++ backends/opencl_backend/test/AnonFuncsTest.exs
@@ -14,41 +14,26 @@ PolyHok.defmodule SimpleTest do
 end
 
 array_size = 100
-
 array_cpu_int = Nx.tensor(Enum.to_list(1..array_size), type: {:s, 32})
-# array_cpu_float = Nx.tensor(Enum.to_list(1..array_size), type: {:f, 32})
 
 IO.inspect(array_cpu_int, label: "CPU Array [int]")
-# IO.inspect(array_cpu_float, label: "CPU Array [float]")
 
 # Create a tensor on the GPU copying the data from the CPU tensor
 array_gpu_int = array_cpu_int |> PolyHok.new_gnx()
-# array_gpu_float = array_cpu_float |> PolyHok.new_gnx()
 
 # Spawn the kernel to run on the GPU
 PolyHok.spawn(
           &SimpleTest.simple_kernel/2,  # Kernel function
           {1, 1, 1},                    # Number of blocks
           {array_size, 1, 1},           # Threads per block
-          [ # Kernel parameters
+          # Kernel parameters
+          [
             array_gpu_int,
             array_size,
             PolyHok.phok(fn x -> x * 2 end)
           ])
 
-# PolyHok.spawn(
-#           &SimpleTest.simple_kernel/2,  # Kernel function
-#           {1, 1, 1},                    # Number of blocks
-#           {array_size, 1, 1},           # Threads per block
-#           [ # Kernel parameters
-#             array_gpu_float,
-#             array_size,
-#             PolyHok.phok(fn x -> x * 2 end)
-#           ])
-
 # Get result back to CPU
 result_int = PolyHok.get_gnx(array_gpu_int)
-# result_float = PolyHok.get_gnx(array_gpu_float)
 
 IO.inspect(result_int, label: "Result after kernel execution [int]")
-# IO.inspect(result_float, label: "Result after kernel execution [float]")

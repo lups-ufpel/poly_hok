ddff8b92fad2cee62de8a91494f1f4ea01a2a282
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-08-14 16:57:04 -0300

    Add comments to clarify usage of runtime.exs for local development

--- backends/opencl_backend/config/runtime.exs
+++ backends/opencl_backend/config/runtime.exs
@@ -1,3 +1,5 @@
 import Config
 
+# This is used for local development and testing. Elixir ignores config/runtime.exs when pulling dependencies.
+# The users have to set their own backend in the config/runtime.exs file. This is intentional behavior.
 config :poly_hok, backend: OpenclBackend

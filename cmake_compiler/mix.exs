da8ef65f05668af5b07bbbb6f474b68b6c47c3dc
Equiel-1703 <henriquegabriel22.hg@gmail.com>
2026-08-15 16:16:16 -0300

    Remove unused deps function and update dependencies in mix.exs

--- cmake_compiler/mix.exs
+++ cmake_compiler/mix.exs
@@ -7,7 +7,7 @@ defmodule CmakeCompiler.MixProject do
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: []
     ]
   end
 
@@ -15,11 +15,6 @@ defmodule CmakeCompiler.MixProject do
   def application do
     [
       extra_applications: [:logger]
-    ]
-  end
-
-  defp deps do
-    [
     ]
   end
 end
